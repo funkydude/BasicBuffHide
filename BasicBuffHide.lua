47dbe9013579c1f28d64ce09e7f39375d68dafef
funkydude <funkydude@users.noreply.github.com>
2025-05-21 15:44:45 +0100

    Don't unregister events, closes #2

--- BasicBuffHide.lua
+++ BasicBuffHide.lua
@@ -5,12 +5,12 @@
 local f = CreateFrame("Frame")
 f:Hide()
 
-f.UnregisterAllEvents(BuffFrame)
+--f.UnregisterAllEvents(BuffFrame) -- Causes issues with Edit Mode on retail
 f.Hide(BuffFrame)
 f.SetParent(BuffFrame, f)
 
 if TemporaryEnchantFrame then -- XXX Dragonflight compat
-	f.UnregisterAllEvents(TemporaryEnchantFrame)
+	--f.UnregisterAllEvents(TemporaryEnchantFrame)
 	f.Hide(TemporaryEnchantFrame)
 	f.SetParent(TemporaryEnchantFrame, f)
 end
